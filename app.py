b69b50fb4d3f5b167dc7b6ea3695549946eb3a1f
AlexnderKukachev <kukachov666@yandex.ru>
2021-04-17 21:06:27 +0900

    added some big changes. commit done to have a save for emergency causes

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
 
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Products %r>' %self.id
 
 @app.route("/")
 @app.route("/home")
